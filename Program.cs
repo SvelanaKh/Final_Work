32dd05b4da49302e54078bdad394722ed4a12c4e
SvelanaKh <109788169+SvelanaKh@users.noreply.github.com>
2022-09-21 10:30:39 +0300

    Добавлена программа

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] startArray = new string[5] {"fff", "oooooo", "dd", "ccccc", "vv"};
+string[] resultArray = new string[startArray.Length];
+
+void ResultArrayWithIf(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultArrayWithIf(startArray, resultArray);
+PrintArray(resultArray);
